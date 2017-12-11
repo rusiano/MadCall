9605875a5826148a02e622e6736ba94503ea059f
rusiano <rusiano@users.noreply.github.com>
2017-12-11 13:57:57 +0100

    Added AWS connection and querying; added markers display on camera move

--- app/src/main/java/com/example/rusia/madcall/MapHelper.java
+++ app/src/main/java/com/example/rusia/madcall/MapHelper.java
@@ -10,9 +10,12 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.v4.content.ContextCompat;
 
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.os.Handler;
+import android.widget.Toast;
 
+import com.flipboard.bottomsheet.BottomSheetLayout;
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
@@ -22,10 +25,18 @@ import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.LatLngBounds;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.hp.hpl.jena.query.Query;
+import com.hp.hpl.jena.query.QueryFactory;
+import com.hp.hpl.jena.query.QuerySolution;
+import com.hp.hpl.jena.query.ResultSet;
+import com.hp.hpl.jena.query.Syntax;
+import com.hp.hpl.jena.sparql.engine.http.QueryEngineHTTP;
 
+import java.util.ArrayList;
 import java.util.Random;
 
 import static com.example.rusia.madcall.MapHelper.CameraConstant.DEFAULT_BOUNDS;
@@ -40,10 +51,17 @@ import static com.example.rusia.madcall.MapHelper.CameraConstant.MIN_LNG;
  * Useful methods to handle MapActivity tasks.
  */
 
-class MapHelper {
+class MapHelper
+        implements  GoogleMap.OnMarkerClickListener,
+        GoogleMap.OnCameraMoveStartedListener,
+        GoogleMap.OnCameraMoveListener,
+        GoogleMap.OnCameraIdleListener{
 
     static final String LOG_TAG = "MADCALL";
+    private static final String AWS_SPARQL_ENDPOINT_URL
+            = "http://ec2-54-208-226-156.compute-1.amazonaws.com/sparql";
     static final int PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION = 1;
+    private static final float DFLT_COMPASS_BTN_ROT = -45f;
 
     interface CameraConstant {
         int    DEFAULT_ZOOM = 17;
@@ -63,19 +81,21 @@ class MapHelper {
     // Google Map API related
     private static Location           mLastKnownLocation;
 
-    private MapsActivity helpedActivity;
+    private MapsActivity activity;
     private Context ctx;
-    private boolean isCameraMapCentered, isCameraMapStraight;
+    private boolean isCameraMapCentered, isCameraMapStraight, wasMapMovedByUser;
 
     private FloatingActionButton mMyLocationButton, mMyOrientationButton;
 
-    MapHelper(MapsActivity helpedActivity) {
-        this.helpedActivity = helpedActivity;
-        this.ctx = helpedActivity.getApplicationContext();
-    }
+    private double currentMinLat;
+    private double currentMaxLat;
+    private double currentMinLng;
+    private double currentMaxLng;
+    private ArrayList<LatLng> markersCoords;
 
-    public FloatingActionButton getmMyOrientationButton() {
-        return mMyOrientationButton;
+    MapHelper(MapsActivity activity) {
+        this.activity = activity;
+        this.ctx = activity.getApplicationContext();
     }
 
     /**
@@ -96,8 +116,8 @@ class MapHelper {
         this.isCameraMapStraight = true;
 
         // Obtain the location and orientation buttons
-        mMyLocationButton = helpedActivity.findViewById(R.id.fab_location);
-        mMyOrientationButton = helpedActivity.findViewById(R.id.fab_orientation);
+        mMyLocationButton = activity.findViewById(R.id.fab_location);
+        mMyOrientationButton = activity.findViewById(R.id.fab_orientation);
 
         // Define the listener for our customized MyLocation button
         mMyLocationButton.setOnClickListener(new View.OnClickListener() {
@@ -129,10 +149,10 @@ class MapHelper {
                 = LocationServices.getFusedLocationProviderClient(ctx);
 
         try {
-            if (helpedActivity.ismLocationPermissionGranted()) {
+            if (activity.ismLocationPermissionGranted()) {
                 Log.wtf(LOG_TAG, "permission was granted");
                 Task<Location> locationResult = mFusedLocationProviderClient.getLastLocation();
-                locationResult.addOnCompleteListener(helpedActivity, new OnCompleteListener<Location>() {
+                locationResult.addOnCompleteListener(activity, new OnCompleteListener<Location>() {
                     @Override
                     public void onComplete(@NonNull Task<Location> task) {
                         mLastKnownLocation = (task.isSuccessful()) ? task.getResult() : null;
@@ -187,9 +207,9 @@ class MapHelper {
      */
     void placeRandomMarkers(GoogleMap mMap, int n) {
         for (int i = 0; i < n; i++) {
-            Double lat = MIN_LAT + (MAX_LAT - MIN_LAT)
+            double lat = MIN_LAT + (MAX_LAT - MIN_LAT)
                     * (double) Math.round((new Random().nextDouble()) * 1000000d) / 1000000d;
-            Double lng = MIN_LNG + (MAX_LNG - MIN_LNG)
+            double lng = MIN_LNG + (MAX_LNG - MIN_LNG)
                     * (double) Math.round((new Random().nextDouble()) * 1000000d) / 1000000d;
             int ic = new Random().nextInt(ICONS.length);
 
@@ -228,7 +248,7 @@ class MapHelper {
         if (isCameraMapCentered) {
             mMyLocationButton.setClickable(false);
             mMyLocationButton.setBackgroundTintList(
-                    helpedActivity.getResources().getColorStateList(R.color.colorPrimaryFaded));
+                    activity.getResources().getColorStateList(R.color.colorPrimaryFaded));
 
             // Make the button disappear after 1 sec if the camera is still straight
             new Handler().postDelayed(new Runnable() {
@@ -242,7 +262,7 @@ class MapHelper {
             mMyLocationButton.setVisibility(View.VISIBLE);
             mMyLocationButton.setClickable(true);
             mMyLocationButton.setBackgroundTintList(
-                    helpedActivity.getResources().getColorStateList(R.color.colorPrimary));
+                    activity.getResources().getColorStateList(R.color.colorPrimary));
         }
     }
 
@@ -257,7 +277,7 @@ class MapHelper {
         if (isCameraMapStraight) {
             mMyOrientationButton.setClickable(false);
             mMyOrientationButton.setBackgroundTintList(
-                    helpedActivity.getResources().getColorStateList(R.color.colorPrimaryFaded));
+                    activity.getResources().getColorStateList(R.color.colorPrimaryFaded));
 
             // Make the button disappear after 1 sec if the camera is still straight
             new Handler().postDelayed(new Runnable() {
@@ -271,7 +291,135 @@ class MapHelper {
             mMyOrientationButton.setVisibility(View.VISIBLE);
             mMyOrientationButton.setClickable(true);
             mMyOrientationButton.setBackgroundTintList(
-                    helpedActivity.getResources().getColorStateList(R.color.colorPrimary));
+                    activity.getResources().getColorStateList(R.color.colorPrimary));
         }
+    }
+
+    ///
+    /// LINSTENER'S METHODS
+    ///
+
+    @Override
+    public void onCameraMoveStarted(int reason) {
+
+        if (reason == GoogleMap.OnCameraMoveStartedListener.REASON_GESTURE) {
+            this.wasMapMovedByUser = true;
+            if (isCameraMapCentered()) {
+                // map is no more centered - highlight myLocationButton
+                setCameraMapCentered(false);
+            }
+        }
+    }
+
+    @Override
+    public void onCameraMove() {
+
+        markersCoords = new ArrayList<>();
+
+        if (MapsActivity.getmMap().getCameraPosition().bearing != 0
+                && this.wasMapMovedByUser) {
+            if (isCameraMapStraight()) {
+                setCameraMapStraight(false);
+                return;
+            }
+            mMyOrientationButton.setRotation(
+                    DFLT_COMPASS_BTN_ROT - MapsActivity.getmMap().getCameraPosition().bearing);
+        }
+    }
+
+    @Override
+    public void onCameraIdle() {
+
+        GoogleMap mMap = MapsActivity.getmMap();
+
+        // if camera was previously moved by the user and the map was rotated, adjust the compass
+        //  icon orientation and save that the camera stopped moving
+        if (mMap.getCameraPosition().bearing != 0 && this.wasMapMovedByUser) {
+            mMyOrientationButton.setRotation(
+                    DFLT_COMPASS_BTN_ROT - MapsActivity.getmMap().getCameraPosition().bearing);
+            this.wasMapMovedByUser = false;
+        }
+
+        // if the map is too zoomed out, then do not show any street
+        if (mMap.getCameraPosition().zoom < DEFAULT_ZOOM) {
+            mMap.clear();
+            return;
+        }
+
+        // otherwise, get the new bounds of the screen and load the streets that are in the
+        //  currently visible region
+        LatLngBounds bounds = mMap.getProjection().getVisibleRegion().latLngBounds;
+
+        currentMinLat = bounds.southwest.latitude;
+        currentMaxLat = bounds.northeast.latitude;
+
+        currentMinLng = bounds.southwest.longitude;
+        currentMaxLng = bounds.northeast.longitude;
+
+        // query for streets whose coordinates lay within the current visible region
+        new Thread( new Runnable() {
+
+            @Override
+            public void run() {
+                try  {
+
+                    String queryString =
+                            "SELECT distinct ?lat ?lng " +
+                                    "where {?street <http://www.semanticweb.org/madcal#latitude> ?lat. " +
+                                    "?street <http://www.semanticweb.org/madcal#longitude> ?lng. " +
+                                    "filter (?lat < " + currentMaxLat + "). " +
+                                    "filter (?lat > " + currentMinLat + "). " +
+                                    "filter (?lng > " + currentMinLng + "). " +
+                                    "filter (?lng < " + currentMaxLng + ") }" ;
+
+                    Query query = QueryFactory.create(queryString, Syntax.syntaxARQ);
+                    QueryEngineHTTP exec = new QueryEngineHTTP(AWS_SPARQL_ENDPOINT_URL, query);
+                    final ResultSet results = exec.execSelect() ;
+
+                    activity.runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            while (results.hasNext()) {
+                                QuerySolution binding = results.nextSolution();
+                                double lat = binding.getLiteral("lat").getDouble();
+                                double lng = binding.getLiteral("lng").getDouble();
+
+                                MapsActivity.getmMap().addMarker(
+                                        new MarkerOptions().position(new LatLng(lat, lng)));
+                            }
+                        }
+                    });
+
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+
+        }).start();
+    }
+
+    /**
+     * This method is called when the user clicks on a marker.
+     */
+    @Override
+    public boolean onMarkerClick(final Marker marker) {
+
+        Context ctx = activity.getApplicationContext();
+        BottomSheetLayout mBottomSheet = activity.findViewById(R.id.bottomsheet);
+
+        // Slide in the infobox (bottomsheet) and inflate the corresponding layout.
+        mBottomSheet.showWithSheetView(LayoutInflater.from(ctx).inflate(
+                R.layout.infobox, mBottomSheet, false));
+
+        Toast.makeText(ctx,
+                "(" + marker.getPosition().latitude + ", " + marker.getPosition().longitude + ")",
+                Toast.LENGTH_SHORT).show();
+
+        //TODO: retrieve information for the appropriate marker and display it in the bottom sheet
+
+        // Return false to indicate that we have not consumed the event and that we wish
+        // for the default behavior to occur (which is for the camera to move such that the
+        // marker is centered and for the marker's info window to open, if it has one).
+        return false;
     }
 }
